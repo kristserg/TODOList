dc02c1a004c26de3e4e6d472b69bdcc30d596947
kristserg <31962426+kristserg@users.noreply.github.com>
2017-12-27 15:36:09 +0200

    Update TODOTester.java

--- tests/TODOTester.java
+++ tests/TODOTester.java
@@ -9,24 +9,8 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 
-public class TODOTester {
-
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
+public class TODOTester 
+{
 	@Test
 	public void testOptionOne() 
 	{
